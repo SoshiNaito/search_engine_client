e052d2e27acb717a1e8e4f66543c38f6c18e5390
内藤 綜志 <naito.soshi@gmail.com>
2019-10-31 16:20:12 +0900

    fix: tmp

--- se-client/src/App.js
+++ se-client/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <p className="pTag">
-        React app
-      </p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="aaa">
+                <p className="pTag">
+                    aaa
+                </p>
+            </div>
+            <div className="bbb">
+                <p className="pTag">
+                    bbb
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
