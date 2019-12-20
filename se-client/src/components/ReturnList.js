fa97738edfaa08d9040f1608b587fd6de94c5057
内藤 綜志 <naito.soshi@gmail.com>
2019-12-20 10:28:52 +0900

    2019/12/20 10:28

---
+++ se-client/src/components/ReturnList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReturnItem from './ReturnItem';
+
+export default class ReturnList extends React.Component {
+
+    // componentWillMount(){                        //やっちゃいいかな。。なくてもいい現段階では
+    //     console.log(this.props.PreviewList)
+    //     this.setState({
+    //         Datalist:[this.props.PreviewList]
+    //     })
+    //     console.log(this.state.Datalist);
+    // }
+
+    render(){
+        
+        return(
+            <div>    
+                 <ul>
+                    {this.props.PreviewList.map((Dlist)=>{      //渡されたのをそのままmap
+                        return(                                 //配列を一つずつ取り出しReturnIitemにDItemとして渡す
+                            <ReturnItem
+                            Ditem={Dlist}
+                            /> 
+                        )
+                    })                
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
