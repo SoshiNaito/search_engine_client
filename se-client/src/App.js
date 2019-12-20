54ea87b94206b3b832520a8e6f63d766f7d8ba7f
unknown <matsuo.emi1130@gmail.com>
2019-12-20 12:14:01 +0900

    fix: Form.js

--- se-client/src/App.js
+++ se-client/src/App.js
@@ -12,8 +12,8 @@ class App extends  React.Component{
     render(){
         return(
             <div className="App">
-                <SerchForm />
-                <PostForm />
+                <PostForm/>
+            	<Form />
             </div>
         )
     }
